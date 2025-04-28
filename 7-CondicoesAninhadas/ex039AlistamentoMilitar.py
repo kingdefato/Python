f980c4bc77973d5194f95a120c6f8f89ccec1d6a
kingdefato <ytallobrito2010@gmail.com>
2025-04-28 15:03:55 -0300

    Resolução de exercicios

--- 7-CondicoesAninhadas/ex039AlistamentoMilitar.py
+++ 7-CondicoesAninhadas/ex039AlistamentoMilitar.py
@@ -7,21 +7,17 @@ print('''Escolha uma das opções abaixo
 opção = int(input('Sua opção: '))
 
 if opção == 1:
-    nascimento = int(input('Ano de nascimento: '))
+    nascimento = int(input('\033[1mAno de nascimento:\033[m '))
     Idade = anoatual - nascimento
     alistamento = 18
-    print('Você tem a obrigação de se alistar para o exercito.')
-    print(
-        f'Quem nasceu no ano de {nascimento} tem {Idade} Anos em {anoatual}.')
+    print('\033[1mAlistamento Obrigatório.\033[m')
+    print(f'Quem nasceu no ano de {nascimento} tem {Idade} Anos em {anoatual}.')
     if Idade > alistamento:
-        print(
-        f'Seu alistamento foi em {nascimento+alistamento}, Você deveria ter se alistado a {Idade-alistamento} Ano(s).')
+        print(f'Seu alistamento foi em {nascimento+alistamento}, Você deveria ter se alistado a {Idade-alistamento} Ano(s).')
     elif Idade < alistamento:
-        print(
-        f'Seu alistamento sera em {nascimento+alistamento}, Você deve se alistar em {alistamento-Idade} Ano(s).')
+        print(f'Seu alistamento sera em {nascimento+alistamento}, Você deve se alistar em {alistamento-Idade} Ano(s).')
     else:
-        print(
-        'Seu alistamento sera esse ano, você deve se alistar \033[1;31mIMEDIATAMENTE!\033[m')
+        print('Seu alistamento sera esse ano, você deve se alistar \033[1;31mIMEDIATAMENTE!\033[m')
 elif opção == 2:
     print('Você não precisa se alistar ao exercito.')
 else: 
