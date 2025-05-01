2611dd366ab1db9b2c52f86a5fc33e2151ddedf6
kingdefato <ytallobrito2010@gmail.com>
2025-05-01 18:26:22 -0300

    Utilizando Módulos

--- 3-Utilizando-Modulos/Aula08-UtilizandoMódulos.py
+++ 3-Utilizando-Modulos/Aula08-UtilizandoMódulos.py
@@ -1,4 +1,4 @@
 import math
 num = int(input('Digite um numero: '))
 raiz = math.sqrt(num)
-print(f'A raiz de {num} é igual a {math.ceil(raiz)}')
+print(f'A raiz de {num} é igual A {math.ceil(raiz)}')
