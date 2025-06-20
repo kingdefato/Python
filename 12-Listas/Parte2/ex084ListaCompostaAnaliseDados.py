d7941376649865b9da7b04f7b09b6ccecbd208a4
kingdefato <ytallobrito2010@gmail.com>
2025-06-20 18:45:03 -0300

    LIstas

--- 12-Listas/Parte2/ex084ListaCompostaAnaliseDados.py
+++ 12-Listas/Parte2/ex084ListaCompostaAnaliseDados.py
@@ -1,23 +1,31 @@
 pessoas = list()
-leves = list()
-pesados = list()
 cadastrogeral = list()
-ncadastros = 0
+mai = men = 0
 while True:
     pessoas.append(str(input('Nome: ')))
     pessoas.append(float(input('Peso: ')))
+    if len(cadastrogeral) == 0:
+        mai = men = pessoas[1]
+    else:
+        if pessoas[1] > mai:
+            mai = pessoas[1]
+        if pessoas[1] < men:
+            men = pessoas[1]
     cadastrogeral.append(pessoas[:])
     pessoas.clear()
-    ncadastros += 1
     go = ' '
     while go not in 'SN':
         go = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
     if 'N' in go:
         break
+print(f'{len(cadastrogeral)} Pessoas foram cadastradas. ')
+print(f'O maior peso foi de {mai}Kg. Peso de ', end= '')
+for p in cadastrogeral:        
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end= '')
+print()
+print(f'O menor peso foi de {men}Kg. Peso de ', end= '')
 for p in cadastrogeral:
-    if p[1] >100:
-        pesados.append(p[0])
-    elif p[1] <70:
-        leves.append(p[0])
-print(f'{ncadastros} Pessoas foram cadastradas. ')
-print(f'As pessoas com os maiores pesos são: {pesados}\nE as com menor peso são: {leves}')
+    if p[1] == men:
+        print(f'[{p[0]}] ', end= '')
+print()
--- 12-Listas/Parte2/ex085ParesImpares.py
+++ 12-Listas/Parte2/ex085ParesImpares.py
@@ -1,9 +1,12 @@
-pares = []
-ímpares = []
-numeros = [[pares], [ímpares]]
+numeros = [[],[]]
 for n in range(1, 8):
     n = int(input(f'Digite o {n}o valor: '))
     if n % 2 == 0:
-        pares.append(n)
+        numeros[0].append(n)
     else:
-        ímpares.append(n)
+        numeros[1].append(n)
+numeros[0].sort()
+numeros[1].sort()
+print('-='*20)
+print(f'Os valores pares são: {numeros[0]}')
+print(f'Os valores ímpares são: {numeros[1]}') 
