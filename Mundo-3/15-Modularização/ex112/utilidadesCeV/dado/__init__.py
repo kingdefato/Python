cd1679415671f181fa258ab6c32b47acb9d1f3a2
kingdefato <ytallobrito2010@gmail.com>
2025-07-18 17:29:40 -0300

    Adiciona a função leiaInt para entrada de números inteiros e melhora a validação na função leiaDinheiro

--- Mundo-3/15-Modularização/ex112/utilidadesCeV/dado/__init__.py
+++ Mundo-3/15-Modularização/ex112/utilidadesCeV/dado/__init__.py
@@ -1,10 +1,25 @@
 def leiaDinheiro(msg):
     ok = False
     while not ok:
-        entrada = str(input(msg)).replace(',','.').strip()
+        entrada = str(input(msg)).replace(',', '.').strip()
         if entrada.isalpha() or entrada == '':
             print(f'\033[31mERRO! {entrada} é um preço inválido.\033[m')
         else:
             valor = float(entrada)
             ok = True
+    return valor
+
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[31mERRO! Digite um número válido.\033[m')
+        if ok:
+            break
     return valor
